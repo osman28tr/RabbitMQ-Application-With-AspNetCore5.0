22c8854c59d75d74f45e009d38c699894d712b3c
osman28tr <osmantonbul2001@gmail.com>
2023-02-19 16:30:06 +0300

    Add project files.

---
+++ UdemyRabbitMQpublisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQpublisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://sessjgkn:r-wEl74R5DKkLtg244A5CBIzpBsunfFD@shark.rmq.cloudamqp.com/sessjgkn");
+
+            using var connection = factory.CreateConnection(); //bağlantı açıldı.
+
+            var channel = connection.CreateModel(); //RabbitMQ ile haberleşmek için bir kanal oluşturuldu.
+
+            channel.QueueDeclare("hello-queue", true/*kuyruk sadece memory'de tutulur ise false yapılır yoksa */, false/*publisher ve subscriber farklı kanallar üzerinden haberleşeceği için aynı kanal olmadığından false yapılır.*/, false/*subscriber silinir ise kuyruk da silinmesini istiyorsak true yapılır.*/); //kuyruk oluşturuldu.
+
+            string message = "hello world";
+
+            var messageBody = Encoding.UTF8.GetBytes(message); //mesajı bytelar halinde göndermek avantajlı olacağından(pdf,word,excel vs.) byte ile gönderdik.
+
+            channel.BasicPublish(string.Empty/*bir exchange yapısı kullanılmadığı zaman varsayılan olarak default exchange olarak adlandırılır.*/, "hello-queue"/*kuyruk ismi ile aynı olmalı*/, null, messageBody);
+
+            Console.WriteLine("Mesaj gönderilmiştir.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQsubscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQsubscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://sessjgkn:r-wEl74R5DKkLtg244A5CBIzpBsunfFD@shark.rmq.cloudamqp.com/sessjgkn");
+
+            using var connection = factory.CreateConnection(); //bağlantı açıldı.
+
+            var channel = connection.CreateModel(); //RabbitMQ ile haberleşmek için bir kanal oluşturuldu.
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true/*subscriber tarafında kuyrukdaki mesaj ulaştı ise bu mesajın direk silinmesini istiyorsak true yaparız. normalde mesajın doğru işlenmeme durumu olduğundan false yapılır.*/, consumer);
+
+            consumer.Received += (object sender,BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Gelen Mesaj: " + message);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
