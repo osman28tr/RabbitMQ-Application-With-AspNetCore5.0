3ba84ea93f46cd3baea82bf9712236df26f673a2
osman28tr <osmantonbul2001@gmail.com>
2023-02-20 16:15:50 +0300

    Work Queue (Corporote Level 2) - Worked with direct exchange. (part: publisher)

--- UdemyRabbitMQpublisher/Program.cs
+++ UdemyRabbitMQpublisher/Program.cs
@@ -5,6 +5,14 @@ using System.Text;
 
 namespace UdemyRabbitMQpublisher
 {
+    public enum LogNames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -16,20 +24,37 @@ namespace UdemyRabbitMQpublisher
 
             var channel = connection.CreateModel(); //RabbitMQ ile haberleşmek için bir kanal oluşturuldu.
 
-            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var queueName = $"direct-queue-{x}";
+
+                var routeKey = $"route-{x}";
+
+                channel.QueueDeclare(queueName, true, false, false);
+
+                channel.QueueBind(queueName, "logs-direct", routeKey,null
+                    );
+            });
+
 
 
             //channel.QueueDeclare("hello-queue", true/*kuyruk sadece memory'de tutulur ise false yapılır yoksa */, false/*publisher ve subscriber farklı kanallar üzerinden haberleşeceği için aynı kanal olmadığından false yapılır.*/, false/*subscriber silinir ise kuyruk da silinmesini istiyorsak true yapılır.*/); //kuyruk oluşturuldu.
 
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-                string message = $"log {x}";
+                LogNames log = (LogNames)new Random().Next(1, 5);
+
+                string message = $"log-type: {log}"; 
 
                 var messageBody = Encoding.UTF8.GetBytes(message); //mesajı bytelar halinde göndermek avantajlı olacağından(pdf,word,excel vs.) byte ile gönderdik.
 
-                channel.BasicPublish("logs-fanout"/*bir exchange yapısı kullanılmadığı zaman varsayılan olarak default exchange olarak adlandırılır.*/, ""/*kuyruk ismi ile aynı olmalı*/, null, messageBody);
+                var routeKey = $"route-{log}";
 
-                Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+                channel.BasicPublish("logs-direct",routeKey,/*bir exchange yapısı kullanılmadığı zaman varsayılan olarak default exchange olarak adlandırılır.*//*kuyruk ismi ile aynı olmalı*/ null, messageBody);
+
+                Console.WriteLine($"Log gönderilmiştir : {message}");
             });
 
             Console.ReadLine();
