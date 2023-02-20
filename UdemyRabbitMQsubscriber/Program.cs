65567b02efe77efa9a5def014469a8f342cc6234
osman28tr <osmantonbul2001@gmail.com>
2023-02-20 13:27:22 +0300

    Work Queue (Corporote Level 2) - Worked with fanout exchange.(part: subscriber)

--- UdemyRabbitMQsubscriber/Program.cs
+++ UdemyRabbitMQsubscriber/Program.cs
@@ -17,11 +17,22 @@ namespace UdemyRabbitMQsubscriber
 
             var channel = connection.CreateModel(); //RabbitMQ ile haberleşmek için bir kanal oluşturuldu.
 
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+            var randomQueueName = channel.QueueDeclare().QueueName; //her subscriber'ın farklı kuyruk ismi olması açısından random bi kuyruk ismi verildi.
+
+            //channel.QueueDeclare(randomQueueName, true, false, false);//kuyruk subscriber down olursa silinmesin.
+
+            channel.QueueBind(randomQueueName,"logs-fanout","",null);//declare edilmedi bind edildi. yani subscriber down olursa kuyruk da silinir. Declare edilirse ilgili subscriber down olursa kuyruk silinmez durur.
+
+
             channel.BasicQos(0, 1, false/*true olur ise toplamda 2. parametredeki değer kadar olacak şekilde aynı anda bütün subscriberlar'a gönderir fakat false olur ise tek seferde bütün subscriberlar'a 2. parametredeki değer kadar mesajları gönderir.*/);
 
-            var consumer = new EventingBasicConsumer(channel);
+            var consumer = new EventingBasicConsumer(channel); //subscriber oluşturuldu.
 
-            channel.BasicConsume("hello-queue", false/*subscriber tarafında kuyrukdaki mesaj ulaştı ise bu mesajın direk silinmesini istiyorsak true yaparız. normalde mesajın doğru işlenmeme durumu olduğundan false yapılır.*/, consumer);
+            channel.BasicConsume(randomQueueName, false/*subscriber tarafında kuyrukdaki mesaj ulaştı ise bu mesajın direk silinmesini istiyorsak true yaparız. normalde mesajın doğru işlenmeme durumu olduğundan false yapılır.*/, consumer);
+
+            Console.WriteLine("Loglar dinleniyor...");
 
             consumer.Received += (object sender,BasicDeliverEventArgs e) =>
             {
